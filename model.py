2ac30646c8580776ff923a213ce763746c8b5db2
Yuhui <yuhuicaowork@gmail.com>
2025-01-28 15:36:18 +1300

    Update model.py
    created cnn model

--- model.py
+++ model.py
@@ -22,3 +22,28 @@ class ANN(nn.Module):
     
     def forward(self, x):
         return self.network(x)
+
+class CNN(nn.Module):
+    def __init__(self, num_class):
+        super(CNN, self).__init__()
+        self.conv_layers = nn.Sequential([
+            nn.Conv2d(1, 32, kernel_size=3, padding=1), 
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        ])
+
+        self.fc_layers = nn.Sequential([
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, num_class),
+            nn.Softmax(dim=1)
+        ])
+
+    def forward(self,x):
+        x = self.conv_layers(x)
+        return self.fc_layers(x)
